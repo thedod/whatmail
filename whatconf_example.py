48c97a7fc9cef0b395230a1137136aadd84f2ffd
The Dod <nonboong@nobita.(none)>
2010-07-24 05:26:53 +0700

    Typo fixes

--- whatconf_example.py
+++ whatconf_example.py
@@ -1,4 +1,4 @@
-DEBUG_TO_WEB=FALSE # set to True when debugging
+DEBUG_TO_WEB=False # set to True when debugging
 SMTP_FROM='myself@gmail.com' # An email address that you're allowed to send from
 SMTP_TO=SMTP_FROM # same address, unless you want to send the form somewhere else
 SMTP_HOST='smtp.gmail.com' # This is for gmail, depends on your mail provider
@@ -9,7 +9,7 @@ SMTP_USERNAME=SMTP_FROM # for gmail (and usually), SMTP_FROM is what you need
 SMTP_PASSWORD='*******'
 SUBJECT_PREFIX='[whatmail] ' # subject line prefix. good for mail filters
 
-# these two you after you register at http://recaptcha.net/api/getkey
+# You get these two at http://recaptcha.net/api/getkey
 RECAPTCHA_PUBLIC_KEY='****************'
 RECAPTCHA_PRIVATE_KEY='***************'
 
