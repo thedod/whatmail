f5da7b26f1f04c961b39ff435dfe32120b44a015
Nimrod S. Kerrett <unclezzzen@gmail.com>
2012-07-22 01:47:07 +0700

    target _blank for ribbon
    :)

--- index.cgi
+++ index.cgi
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This wrapper is not necessarily needed, but some places (e.g. webffaction)
-# require extra env etc. for gpgme to work, so here you can write here:
+# require extra env etc. for gpgme to work, so here you can write something like:
 #   LD_LIBRARY_PATH=/path/to/lib /path/to/my/python whatmail.py
 # Normally, this should work:
 env python whatmail.py
--- whatmail.py
+++ whatmail.py
@@ -44,7 +44,7 @@ FORM_PAGE_TEMPLATE="""<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "
           <p><input type="submit" value="Send"></p>
       </div>
   </form>
-  <a href="https://github.com/thedod/whatmail/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
+  <a target="_blank" href="https://github.com/thedod/whatmail/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
 </body>
 </html>"""
 
